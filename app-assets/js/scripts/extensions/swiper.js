1d1f0eb98ffe313f294e73cd883e26d2d5c3c71c
wen <wen09210>
2020-03-22 16:19:52 +0800

    調整線

--- app-assets/js/scripts/extensions/swiper.js
+++ app-assets/js/scripts/extensions/swiper.js
@@ -131,7 +131,7 @@ $(document).ready(function () {
         spaceBetween: 30,
         centeredSlides: true,
         autoplay: {
-            delay: 2500,
+            delay: 3000,
             disableOnInteraction: false,
         },
         pagination: {
