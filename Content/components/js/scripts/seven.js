fdab9d25f754c7efc3b35512d357dd6c76bf16f4
wen <wen09210>
2020-09-10 23:30:55 +0800

    元件頁增加清單

--- Content/components/js/scripts/seven.js
+++ Content/components/js/scripts/seven.js
@@ -23,10 +23,20 @@ $(function () {
 
         }, 500);
     });
+
+    var buyer_list_number =2;
+    $('#add_buyer_list').click(function () {
+        if (buyer_list_number < 6){
+            $(this).parent().prev().append(' <div class="buyer-list"><div class="buyer-list-number">' + buyer_list_number + '</div><input type="text" class="form-control" name=""placeholder="最多五則，可要求買家填入ATM匯款帳號" ></div>');
+        }
+        buyer_list_number = buyer_list_number + 1;
+        
+    })
 })
 
 $('.mobile-hidden-accordin').click(function () {
     var el = $('.mobile-hidden-accordin .collapse-title span');
     el.text(el.text() == '收合' ? '全部展開' : '收合');
 })
+
 
