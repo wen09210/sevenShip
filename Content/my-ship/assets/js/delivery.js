3b1f351c562f9a1fdd0976e3c6fbb1dd5a68a208
wen <wen09210>
2020-11-08 23:10:58 +0800

    差選單與側邊

--- Content/my-ship/assets/js/delivery.js
+++ Content/my-ship/assets/js/delivery.js
@@ -23,16 +23,25 @@
                 cssEase: false,
                 touchMove: false,
             });
-            $('.actitvity_wrap').slick({
+            $('.actitvity_wrap,.best_seller').slick({
                 autoplay: false,
                 autoplaySpeed: 5000,
                 slidesToShow: 1,
                 slidesToScroll: 1,
                 dots: false,
                 arrows: true,
-                
+                responsive: [
+                    {
+                        breakpoint: 960,
+                        settings: {
+                            dots: true,
+                            arrows: false,
+                        }
+                    },
+                ]    
             });
 
+            
         });
         // sideFunctionClose
 
@@ -52,3 +61,41 @@
         $('button.closeBtn').click(function(){
             $('.global-alert').remove()
         })
+$(document).ready(function () {
+    $('body').scrollspy({
+        target: '#side-nav'
+    });
+});
+$('[data-id="js-burger"]').on('click', function () {
+    $('body').toggleClass('is-openMenu');
+    $('.header').toggleClass('on');
+});
+
+$('.o-menuMask').on('touchstart click', function (e) {
+    e.preventDefault();
+    $('body').removeClass('is-openMenu');
+    $('.header').removeClass('on');
+});
+
+$('.c-menuLv1__name').on('click', function () {
+    $(this).toggleClass('on');
+});
+
+$('.c-menuLv2__name').on('click', function () {
+    $(this).toggleClass('on');
+});
+// 漢堡連結
+$('.cs-o-link').on('touchstart click', function (e) {
+    e.preventDefault();
+    $('body').removeClass('is-openMenu');
+    $('.header').removeClass('on');
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $($(this).attr("href")).offset().top
+    }, 1000);
+});
+
+$('.side-nav .nav-link').on('touchstart click', function () {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $($(this).attr("href")).offset().top
+    }, 1000);
+});
